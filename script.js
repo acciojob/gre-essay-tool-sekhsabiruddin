aa3a76805b638e372fc0c40f0e04a3fe0eb1281e
sk sabiruddin <120248500+sekhsabiruddin@users.noreply.github.com>
2023-01-26 22:50:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your code here
+let essay = document.getElementById('evaluatedText');
+
+
+essay.addEventListener('keyup', ()=>{
+    let count = document.getElementById('wordCount');
+    let text = essay.value;
+    let length = wordCounter(text);
+
+    count.innerHTML = length;
+})
+
+function wordCounter(line){
+    let space = 1;
+    let count = 0;
+    for(let i=0; i<line.length; i++ ){
+        let c = line.charAt(i);
+        console.log(space);
+        if(space == 0 && c == " "){
+            count++;
+            space++;
+        }else if(c != " "){
+            space = 0;
+        }
+        if(i==line.length-1 && c != " "){
+            count++;
+        }
+    }
+
+    return count;
+}
+
